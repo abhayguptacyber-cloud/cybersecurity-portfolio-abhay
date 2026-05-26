7d507be519753eda76ccaaf0284d4e38460c3712
abhayguptacyber-cloud <abhaygupta.cyber@gmail.com>
2026-05-26 13:19:37 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("light-mode");
+
+  if(document.body.classList.contains("light-mode")){
+
+    toggleBtn.textContent = "☀️";
+
+  }else{
+
+    toggleBtn.textContent = "🌙";
+  }
+});
